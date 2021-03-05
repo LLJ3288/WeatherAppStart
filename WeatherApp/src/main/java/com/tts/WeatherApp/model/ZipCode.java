716088ee147d5e2f081eb253546bc511e7c9896e
LLJ3288 <74459081+LLJ3288@users.noreply.github.com>
2021-03-05 11:07:32 -0600

    Add files via upload

--- WeatherApp/src/main/java/com/tts/WeatherApp/model/ZipCode.java
+++ WeatherApp/src/main/java/com/tts/WeatherApp/model/ZipCode.java
@@ -1,11 +1,40 @@
 package com.tts.WeatherApp.model;
 
 
+import lombok.Data;
+
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//@Data           //added for step
+//@Entity
+//public class ZipCode {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+////    public ZipCode() {
+////
+////    }
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class ZipCode {
 
@@ -13,8 +42,10 @@ public class ZipCode {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    public ZipCode() {
+    @CreationTimestamp
+    private Date createdAt;
 
-    }
-
+    private String zipCode;
 }
+
+
